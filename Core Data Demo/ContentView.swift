1d641ca5de31cc85583e4d90a1dfe84a9ab2e1d9
sydbang <sydney.bang94@gmail.com>
2022-05-11 09:26:14 +0900

    add, update and delete data in core data
    - add, update and delete data and save in core data
    - create preview specific instance and path for dummy data

--- Core Data Demo/ContentView.swift
+++ Core Data Demo/ContentView.swift
@@ -10,11 +10,6 @@ import CoreData
 
 struct ContentView: View {
     @Environment(\.managedObjectContext) private var viewContext
-
-    @FetchRequest(
-        sortDescriptors: [NSSortDescriptor(keyPath: \Item.timestamp, ascending: true)],
-        animation: .default)
-    private var items: FetchedResults<Item>
     
     @FetchRequest(sortDescriptors: []) var people: FetchedResults<Person>
 
@@ -28,6 +23,13 @@ struct ContentView: View {
             List {
                 ForEach(people) { person in
                     Text(person.name ?? "No name")
+                        .onTapGesture {
+                            //person.name = "Joe"
+                            //try! viewContext.save()
+                            
+                            viewContext.delete(person)
+                            try! viewContext.save()
+                        }
                 }
             }
         }
@@ -80,21 +82,6 @@ struct ContentView: View {
 //            }
 //        }
     }
-
-    private func deleteItems(offsets: IndexSet) {
-        withAnimation {
-            offsets.map { items[$0] }.forEach(viewContext.delete)
-
-            do {
-                try viewContext.save()
-            } catch {
-                // Replace this implementation with code to handle the error appropriately.
-                // fatalError() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development.
-                let nsError = error as NSError
-                fatalError("Unresolved error \(nsError), \(nsError.userInfo)")
-            }
-        }
-    }
 }
 
 private let itemFormatter: DateFormatter = {
@@ -104,8 +91,8 @@ private let itemFormatter: DateFormatter = {
     return formatter
 }()
 
-//struct ContentView_Previews: PreviewProvider {
-//    static var previews: some View {
-//        ContentView().environment(\.managedObjectContext, PersistenceController.preview.container.viewContext)
-//    }
-//}
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView().environment(\.managedObjectContext, PersistenceController.preview.container.viewContext)
+    }
+}
